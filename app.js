56514d3654450aee81358268fd8af594de64e606
Darts, Adam (ELS) <a.darts@elsevier.com>
2017-09-13 13:53:33 +0100

    Task 2

---
+++ app.js
@@ -0,0 +1,7 @@
+var myVariable = "some String";
+myVariable = 123;
+const myUnchangingValue = "some other string";
+let blockScopedVariable = "whatever";
+console.log(myVariable);
+console.log(myUnchangingValue);
+console.log(blockScopedVariable);
