1a0397ab9502f6ac82e53cb97299871a25fc17e1
Darts, Adam (ELS) <a.darts@elsevier.com>
2017-09-13 15:26:33 +0100

    Task 5

--- app.js
+++ app.js
@@ -10,3 +10,18 @@ const rootContainer = document.getElementById("app-root");
 const paragraphIntro = document.createElement("p");
 paragraphIntro.innerText = "Welcome to BlueBook";
 rootContainer.appendChild(paragraphIntro);
+
+const posts = [
+    {
+        userId: 34,
+        id: 1234,
+        title: "my first post",
+        body: "some totally awesome post content"
+    },
+    {
+        userId: 34,
+        id: 1235,
+        title: "my second post",
+        body: "some other totally awesome post content"
+    }
+];
