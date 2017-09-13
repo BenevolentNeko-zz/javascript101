e84bfb6c84e3936ddb82c9a6dfc49c4d5ba887c0
Darts, Adam (ELS) <a.darts@elsevier.com>
2017-09-13 15:16:50 +0100

    Task 4

--- app.js
+++ app.js
@@ -5,3 +5,8 @@ let blockScopedVariable = "whatever";
 console.log(myVariable);
 console.log(myUnchangingValue);
 console.log(blockScopedVariable);
+
+const rootContainer = document.getElementById("app-root");
+const paragraphIntro = document.createElement("p");
+paragraphIntro.innerText = "Welcome to BlueBook";
+rootContainer.appendChild(paragraphIntro);
